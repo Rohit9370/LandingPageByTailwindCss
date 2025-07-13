27b0c6805700cd064714ac81aafa8608affebc29
SIMon78op <ingalesoyam@gmail.com>
2025-07-13 13:48:35 +0530

    hero section is updated and now its more responsive

--- Script.js
+++ Script.js
@@ -1,15 +1,9 @@
- function MenuShow() {
-        const responsiveDiv = document.getElementById("responsive_div");
-        const expertise = document.getElementById("expertise");
-        const nav = document.getElementById("nav");
+function MenuShow() {
+  const responsiveDiv = document.getElementById("responsive_div");
 
-        if (responsiveDiv.classList.contains("hidden")) {
-          responsiveDiv.classList.remove("hidden");
-          expertise.classList.remove("hidden");
-          responsiveDiv.appendChild(expertise); // optional: show in menu
-        } else {
-          responsiveDiv.classList.add("hidden");
-          expertise.classList.add("hidden");
-          nav.appendChild(expertise); // return to nav
-        }
-      }
+  if (responsiveDiv.classList.contains("hidden")) {
+    responsiveDiv.classList.remove("hidden");
+  } else {
+    responsiveDiv.classList.add("hidden");
+  }
+}
